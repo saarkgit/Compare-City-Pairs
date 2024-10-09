530555e714d396f4d7ca2b4f11b248349c76ac12
saarkgit <saar.kimhi@outlook.com>
2024-10-08 19:45:03 -0700

    working frontend

--- capital_pairs_compare.py
+++ capital_pairs_compare.py
@@ -102,40 +102,46 @@ def draw_connections(
 
 
 def check_for_answer(
-    df_first_pair_of_cities, df_second_pair_of_cities, pair_one_dist, pair_two_dist
+    df_first_pair_of_cities,
+    df_second_pair_of_cities,
+    pair_one_dist,
+    pair_two_dist,
+    user_answer,
 ):
-    print(f"Which capital cities are closer together? (a or b)")
-    user_answer = input(
-        f"a: ({df_first_pair_of_cities['city'].iloc[0]}, {df_first_pair_of_cities['city'].iloc[1]})"
-        + f" or b: ({df_second_pair_of_cities['city'].iloc[0]}, {df_second_pair_of_cities['city'].iloc[1]})\n"
-    )
+    # print(f"Which capital cities are closer together? (a or b)")
+    # user_answer = input(
+    #     f"a: ({df_first_pair_of_cities['city'].iloc[0]}, {df_first_pair_of_cities['city'].iloc[1]})"
+    #     + f" or b: ({df_second_pair_of_cities['city'].iloc[0]}, {df_second_pair_of_cities['city'].iloc[1]})\n"
+    # )
 
-    invalid_answer = True
-    while invalid_answer:
-        if user_answer == "a" or user_answer == "b":
-            invalid_answer = False
-        else:
-            user_answer = input("Please input a valid response.")
+    # invalid_answer = True
+    # while invalid_answer:
+    #     if user_answer == "a" or user_answer == "b":
+    #         invalid_answer = False
+    #     else:
+    #         user_answer = input("Please input a valid response.")
 
-    user_correct = False
+    user_correctness = False
     if user_answer == "a":
-        user_correct = pair_one_dist < pair_two_dist
+        user_correctness = pair_one_dist < pair_two_dist
     else:
-        user_correct = pair_two_dist < pair_one_dist
+        user_correctness = pair_two_dist < pair_one_dist
 
-    if user_correct:
-        print("You are correct!")
-    else:
-        print("Better luck next time!")
+    # if user_correct:
+    #     print("You are correct!")
+    # else:
+    #     print("Better luck next time!")
 
-    print(
-        f"The capitals {df_first_pair_of_cities['city'].iloc[0]} and {df_first_pair_of_cities['city'].iloc[1]} are {pair_one_dist} away."
-        + f"\nWhilst {df_second_pair_of_cities['city'].iloc[0]} and {df_second_pair_of_cities['city'].iloc[1]} are {pair_two_dist} away."
-    )
+    # print(
+    #     f"The capitals {df_first_pair_of_cities['city'].iloc[0]} and {df_first_pair_of_cities['city'].iloc[1]} are {pair_one_dist} away."
+    #     + f"\nWhilst {df_second_pair_of_cities['city'].iloc[0]} and {df_second_pair_of_cities['city'].iloc[1]} are {pair_two_dist} away."
+    # )
 
-    draw_connections(
-        df_first_pair_of_cities, df_second_pair_of_cities, pair_one_dist, pair_two_dist
-    )
+    return user_correctness
+
+    # draw_connections(
+    #     df_first_pair_of_cities, df_second_pair_of_cities, pair_one_dist, pair_two_dist
+    # )
 
 
 # csv_file = "capital_cities_fixed.csv"
--- gui.py
+++ gui.py
@@ -1,10 +1,52 @@
 import tkinter as tk
-from tkinter import messagebox
-import pandas as pd
-import plotly.graph_objects as go
-from capital_pairs_compare import main
+from capital_pairs_compare import main, check_for_answer, draw_connections
+
 # Function to create and show the Plotly map for the first pair of cities
-df_first_pair_of_cities, df_second_pair_of_cities, pair_one_dist, pair_two_dist = main("capital_cities_fixed.csv")
+df_first_pair_of_cities, df_second_pair_of_cities, pair_one_dist, pair_two_dist = main(
+    "capital_cities_fixed.csv"
+)
+
+
+def set_correct_value_label(correct):
+    for widget in root.winfo_children():  # Remove previous widgets
+        widget.destroy()
+
+    if correct:
+        label = tk.Label(
+            root, text="You are correct!", font=("Helvetica", 26), fg="green"
+        )
+        label.pack(pady=50)
+    else:
+        label = tk.Label(
+            root, text="Better luck next time!", font=("Helvetica", 26), fg="red"
+        )
+    
+    label.pack(pady=50)
+
+    button3 = tk.Button(
+        root,
+        text="See distances on a globe?",
+        font=("Helvetica", 22),
+        command=lambda: draw_connections(
+            df_first_pair_of_cities,
+            df_second_pair_of_cities,
+            pair_one_dist,
+            pair_two_dist,
+        ),
+    )
+    button3.pack(pady=30)
+
+
+def get_user_answer(answer):
+    user_correct_bool = check_for_answer(
+        df_first_pair_of_cities,
+        df_second_pair_of_cities,
+        pair_one_dist,
+        pair_two_dist,
+        answer,
+    )
+
+    set_correct_value_label(user_correct_bool)
 
 
 # Create the main application window
@@ -17,12 +59,23 @@ label = tk.Label(root, text="Compare City-Pairs", font=("Helvetica", 32))
 label.pack(pady=20)
 
 # Create the first button
-button1 = tk.Button(root, text=f"{df_first_pair_of_cities.iloc[0]['city']} to {df_first_pair_of_cities.iloc[1]['city']}", font=("Helvetica", 22))#, command=show_first_pair_map)
+button1 = tk.Button(
+    root,
+    text=f"{df_first_pair_of_cities.iloc[0]['city']} to {df_first_pair_of_cities.iloc[1]['city']}",
+    font=("Helvetica", 22),
+    command=lambda: get_user_answer("a"),
+)
 button1.pack(pady=30)
 
 # Create the second button
-button2 = tk.Button(root, text=f"{df_second_pair_of_cities.iloc[0]['city']} to {df_second_pair_of_cities.iloc[1]['city']}", font=("Helvetica", 22))#, command=show_second_pair_map)
+button2 = tk.Button(
+    root,
+    text=f"{df_second_pair_of_cities.iloc[0]['city']} to {df_second_pair_of_cities.iloc[1]['city']}",
+    font=("Helvetica", 22),
+    command=lambda: get_user_answer("b"),
+)
 button2.pack(pady=10)
 
 # Start the Tkinter event loop
 root.mainloop()
+# RESET GAME
